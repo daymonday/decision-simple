ac8d7121c60376417aeb8279c8f5c86878c8a27d
Joaquín Krasuk <joaquin.krasuk@albany.company>
2021-04-24 18:16:05 -0300

    Ejercicio 7

--- 4-7.c
+++ 4-7.c
@@ -2,17 +2,21 @@
 
 int main()
 {
-    printf("Ingrese tres numeros positivos:");
-    scanf("%d",n1);
-    printf("Ingrese tres numeros positivos:");
-    scanf("%d",n2);
-    printf("Ingrese tres numeros positivos:");
-    scanf("%d",n3);
-    if
-    {n3==n1+n2
-    printf("Determina triangulo");
+    int n1,n2,n3;
+    printf("\nIngrese tres numeros positivos:");
+    scanf("%d",&n1);
+    printf("\nIngrese tres numeros positivos:");
+    scanf("%d",&n2);
+    printf("\nIngrese tres numeros positivos:");
+    scanf("%d",&n3);
+
+    if(n3==n1+n2)
+    {
+        printf("\nDetermina triangulo\n");
+    }
     else
-        printf("No determina triangulo");
+    {
+        printf("\nNo determina triangulo\n");
     }
     return 0;
 }
