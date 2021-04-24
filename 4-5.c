695e8358606578bbee06014e4edfa5dca1f03b76
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 10:00:59 -0300

    Update 4-5.c

--- 4-5.c
+++ 4-5.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 
 int main ()
-
-int
-printf("Ingrese 3 numeros:"nn);
+{
+    printf("Ingrese 3 numeros:"nn);
 scanf("%d",&N1);
 printf("Ingrese otro numero:");
 scanf("%d",&N2);
 printf("Ingrese otro numero:");
-scanf("%d",&N3);
-if(N1>N2 && N1>N3);
-    uno = N1
-    if N2 > N3
-    dos = N2
-    tres = N3
+scanf("%d,&N3);
+if(N1>N2&&N1>N3);
+      {uno=N1
+    if 
+      {N2 > N3
+    dos=N2
+    tres=N3
         else
-            dos = N3
-            tres = N2
+            dos=N3
+            tres=N2
+          }
     else
-        N2> N1 && N2>N3
-        uno = N2
-            if N1 > N3
-                dos = N1
-                tres = N3
+    { N2>N1&& 2>N3
+        uno=N2
+    {if N1 > N3
+                dos=N1
+                tres=N3
             else
-                dos = N3
-                tres = N1
+                dos=N3
+                tres=N1}
+    }
 else
-    uno = N3
-     if N2 > N3
-                dos = N2
-                tres = N3
+    uno=N3
+{if N2>N3
+                dos=N2
+                tres=N3
             else
-                dos = N3
-                tres = N2
-
-printf("El orden es: %d, %d, %d",uno,dos,tres);
+                dos=N3
+                tres=N2}
+      }
+printf("El orden es: %d,%d,%d",uno,dos,tres);
 
 system ("pause");
 return 0;
-
+      }
