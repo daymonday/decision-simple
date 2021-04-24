9a854dea75494ce02beae37c82e787e940e01980
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 19:32:05 -0300

    Update 4-7.c

--- 4-7.c
+++ 4-7.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-int main()
+int
+main ()
 {
-    printf("Ingrese tres numeros positivos:");
-    scanf("%d",n1);
-    printf("Ingrese tres numeros positivos:");
-    scanf("%d",n2);
-    printf("Ingrese tres numeros positivos:");
-    scanf("%d",n3);
-    if
-    {n3==n1+n2
-    printf("Determina triangulo");
-    else
-        printf("No determina triangulo");
+  int n1, n2, n3;
+  printf ("\nIngrese un numero positivo:\n");
+  scanf ("%d",&n1);
+  printf ("\nIngrese otro numero positivo:\n");
+  scanf ("%d",&n2);
+  printf ("\nIngrese el ultimo numero positivo:\n");
+  scanf ("%d",&n3);
+  if (n3 == n1 + n2)
+    {
+      printf ("\nDetermina triangulo");
     }
-    return 0;
+  else
+    {
+      printf ("\nNo determina triangulo\n");
+    }
+  return 0;
 }
-
