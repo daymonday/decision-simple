a613fbf628c37957831dd8bac3d69702f4af621b
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 10:05:18 -0300

    Update 4-2.c

--- 4-2.c
+++ 4-2.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 int main ()
-
-int
-printf("Ingrese 3 numeros:"nn);
+{
+    printf("Ingrese 3 numeros:"nn);
 scanf("%d",&N1);
 printf("Ingrese otro numero:");
 scanf("%d",&N2);
 printf("Ingrese otro numero:");
 scanf("%d",&N3);
-if(N1<N2 && N1<N3);
-    printf("N1%d es menor");
+if(N1<N2&&N1<N3);
+    { { printf("%d es menor",N1);
     else
-        N2<N1 && N2<N3
-        printf("N2%d es menor");
+        N2<N1&&N2<N3
+        printf("%d es menor",N2);
+    }
 else
-    printf("N3%d es menor");
+    printf("%d es menor",N3);
+}
 system ("pause");
 return 0;
+}
