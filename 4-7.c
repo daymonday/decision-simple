ad0aff5cafb9c5263cb923a17f7acd4445279f04
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 10:22:09 -0300

    Update 4-7.c

--- 4-7.c
+++ 4-7.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ingrese tres numeros positivos:");
+    scanf("%d",n1);
+    printf("Ingrese tres numeros positivos:");
+    scanf("%d",n2);
+    printf("Ingrese tres numeros positivos:");
+    scanf("%d",n3);
+    if
+    {n3==n1+n2
+    printf("Determina triangulo");
+    else
+        printf("No determina triangulo");
+    }
+    return 0;
+}
 
