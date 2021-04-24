9a9ddd24b9f0fc99a56246d216d370a1d289d7ec
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 10:22:40 -0300

    Update 4-8.c

--- 4-8.c
+++ 4-8.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ingrese dos numeros enteros:");
+    scanf("%d",n1);
+    printf("Ingrese dos numeros enteros");
+    scanf("%d",n2);
+    if
+    {n1/n2
+    printf("Es divisible");
+    else
+        printf("No es divisible");
+    }
+    return 0;
+}
+
 
