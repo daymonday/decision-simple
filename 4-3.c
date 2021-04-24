517ed72502e2a9748bbed0800b9d8ef4d08ff5ba
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 19:01:54 -0300

    Update 4-3.c

--- 4-3.c
+++ 4-3.c
@@ -1,22 +1,32 @@
-#include <stdio.h>
-
-int main()
+int
+main ()
 {
-    printf("Ingrese 3 numeros:"nn);
-    scanf("%d",&N1);
-    printf("Ingrese otro numero:");
-    scanf("%d",&N2);
-    printf("Ingrese otro numero:");
-    scanf("%d",&N3);
-    if(N1==N2+N3);
-    {   { printf("%d es el producto de N2 y N3",N1);
-            else
-                N2==N1+N3
-                printf("%d es el producto de N1 y N3",N2);
-        }
-    else
-        printf("%d es el producto de N1 y N2",N3);
+  int N1, N2, N3;
+  printf ("\nIngrese 3 numeros:\n\n");
+  scanf ("%d", &N1);
+  printf ("\nIngrese otro numero:\n\n");
+  scanf ("%d", &N2);
+  printf ("\nIngrese otro numero:\n\n");
+  scanf ("%d", &N3);
+  if (N1 == N2 + N3)
+    {
+      printf ("%d es el producto de N2 y N3", N1);
     }
-system ("pause");
-return 0;
+  else if (N2 == N1 + N3)
+    {
+      printf ("%d es el producto de N1 y N3", N2);
+    }
+
+  else if (N2 == N1 + N3)
+    {
+      printf ("%d es el producto de N1 y N3", N2);
+    }
+
+  else
+    {
+      printf ("%d es el producto de N1 y N2", N3);
+      printf ("%d es el producto de N1 y N2", N3);
+    }
+
+  return 0;
 }
