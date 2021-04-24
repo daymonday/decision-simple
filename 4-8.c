0a955addf688f535f9c671c75dc90ecf2ed2a858
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 19:35:42 -0300

    Update 4-8.c
    Si funciona pero no estoy segura si el problema me preguntaba si se podian dividir numeros enteros ????

--- 4-8.c
+++ 4-8.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main()
-{
-    printf("Ingrese dos numeros enteros:");
-    scanf("%d",n1);
-    printf("Ingrese dos numeros enteros");
-    scanf("%d",n2);
-    if
-    {n1/n2
+{ int n1,n2;
+    printf("\nIngrese un numero entero:\n");
+    scanf("%d",&n1);
+    printf("\nIngrese otro numero entero:\n");
+    scanf("%d",&n2);
+    if(n1/n2)
+    {
     printf("Es divisible");
+    }
     else
+    {
         printf("No es divisible");
     }
     return 0;
