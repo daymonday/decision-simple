a3b0e1931a69e73bc740daecd76a3e683122b408
Joaquín Krasuk <joaquin.krasuk@albany.company>
2021-04-24 18:17:51 -0300

    Ejercicio 8

--- 4-8.c
+++ 4-8.c
@@ -2,17 +2,19 @@
 
 int main()
 {
-    printf("Ingrese dos numeros enteros:");
-    scanf("%d",n1);
-    printf("Ingrese dos numeros enteros");
-    scanf("%d",n2);
-    if
-    {n1/n2
-    printf("Es divisible");
-    else
-        printf("No es divisible");
+    int n1,n2;
+    printf("\nIngrese dos numeros enteros:");
+    scanf("%d",&n1);
+    printf("\nIngrese dos numeros enteros:");
+    scanf("%d",&n2);
+    if(n1%n2==0)
+    {
+        printf("\nEs divisible");
     }
+    else
+    {
+        printf("\nNo es divisible");
+    }
+
     return 0;
 }
-
-
