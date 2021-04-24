84ba09911b16a04785b74559caa287f37ab54315
Joaquín Krasuk <joaquin.krasuk@albany.company>
2021-04-24 18:08:21 -0300

    Fix ejercicio 3

--- 4-3.c
+++ 4-3.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-    printf("Ingrese 3 numeros:"nn);
+    int N1, N2, N3;
+    printf("\nIngrese 3 numeros:");
     scanf("%d",&N1);
-    printf("Ingrese otro numero:");
+    printf("\nIngrese otro numero:");
     scanf("%d",&N2);
-    printf("Ingrese otro numero:");
+    printf("\nIngrese otro numero:");
     scanf("%d",&N3);
-    if(N1==N2+N3);
-    {   { printf("%d es el producto de N2 y N3",N1);
-            else
-                N2==N1+N3
-                printf("%d es el producto de N1 y N3",N2);
-        }
-    else
-        printf("%d es el producto de N1 y N2",N3);
+    if(N1==N2*N3)
+    {
+        printf("\n%d es el producto de N2 y N3\n",N1);
     }
-system ("pause");
-return 0;
+    else if(N2==N1*N3)
+    {
+        printf("\n%d es el producto de N1 y N3\n",N2);
+    }
+    else if(N3==N2*N1)
+    {
+        printf("\n%d es el producto de N1 y N2\n",N3);
+    }
+
+    system ("pause");
+    return 0;
 }
