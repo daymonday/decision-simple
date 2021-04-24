83921348e523d056c8593c47c2b5d4377d0d619a
Joaquín Krasuk <joaquin.krasuk@albany.company>
2021-04-24 18:14:24 -0300

    Ejercicio 5

--- 4-5.c
+++ 4-5.c
@@ -1,44 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
-    printf("Ingrese 3 numeros:"nn);
-scanf("%d",&N1);
-printf("Ingrese otro numero:");
-scanf("%d",&N2);
-printf("Ingrese otro numero:");
-scanf("%d,&N3);
-if(N1>N2&&N1>N3);
-      {uno=N1
-    if 
-      {N2 > N3
-    dos=N2
-    tres=N3
-        else
-            dos=N3
-            tres=N2
-          }
-    else
-    { N2>N1&& 2>N3
-        uno=N2
-    {if N1 > N3
-                dos=N1
-                tres=N3
-            else
-                dos=N3
-                tres=N1}
-    }
-else
-    uno=N3
-{if N2>N3
-                dos=N2
-                tres=N3
-            else
-                dos=N3
-                tres=N2}
-      }
-printf("El orden es: %d,%d,%d",uno,dos,tres);
+    int N1, N2, N3;
+    int uno, dos,tres;
+    printf("\nIngrese 3 numeros:");
+    scanf("%d",&N1);
+    printf("\nIngrese otro numero:");
+    scanf("%d",&N2);
+    printf("\nIngrese otro numero:");
+    scanf("%d",&N3);
 
-system ("pause");
-return 0;
-      }
+    if(N1>N2&&N1>N3)
+    {
+        uno=N1;
+        if(N2 > N3)
+        {
+
+            dos=N2;
+            tres=N3;
+        }
+        else
+        {
+
+            dos=N3;
+            tres=N2;
+        }
+    }
+    else if(N2>N1&& 2>N3)
+    {
+        uno=N2;
+
+        if (N1 > N3)
+        {
+            dos=N1;
+            tres=N3;
+        }
+        else
+        {
+            dos=N3;
+            tres=N1;
+        }
+    }
+    else
+    {
+        uno=N3;
+        if (N2>N3)
+        {
+            dos=N2;
+            tres=N3;
+        }
+        else
+        {
+            dos=N3;
+            tres=N2;
+        }
+    }
+
+    printf("\nEl orden es: %d,%d,%d\n",uno,dos,tres);
+
+    system ("pause");
+    return 0;
+}
