88b65846157fba82eb2511efdb03a41edb2d290e
dia lunes <82815332+daymonday@users.noreply.github.com>
2021-04-24 20:00:36 -0300

    Update 4-12.c

--- 4-12.c
+++ 4-12.c
@@ -1,22 +1,34 @@
-#define bono50=500
-#define bono150=1000
+
 #include <stdio.h>
-
-main ()
+  
+int
+main () 
 {
-    printf("Ingrese valor de hora trabajadas:");
-    scanf("%d"&valor_hora)
-    printf("Ingrese cantidad de horas trabajadas:");
-    scanf("%d"&cant_hora);
-    sueldo=valor_hora*cant_hora
-
-    if cant_hora>50
-    {
-        if cant_hora>150
-            printf("El sueldo es %d",sueldo+bono50+bono150);
-        else printf("El sueldo es %d",sueldo+bono50);
+  int valor_hora, cant_hora, bono50, bono150, sueldo;
+  
+ 
+bono50 = 500;
+bono150 = 1000;
+printf ("Ingrese valor de hora trabajadas:");
+scanf ("%d", &valor_hora);
+printf ("Ingrese cantidad de horas trabajadas:");
+scanf ("%d", &cant_hora);
+sueldo = valor_hora * cant_hora;
+if (cant_hora > 50)
+     {
+      printf ("El sueldo es %d", sueldo + bono50);
     }
-    else printf("El sueldo es %d",sueldo);
-
-    return 0;
+  else if(cant_hora>150) 
+  { 
+      printf ("El sueldo es %d", sueldo + bono50 + bono150);
+       }
+else 
+{
+    printf ("El sueldo es %d", sueldo);
+    
 }
+
+return 0;
+ 
+}
+
