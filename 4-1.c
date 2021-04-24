7be3f06ce58b70a5319dd76649ef9791d05e67e0
Joaquín Krasuk <joaquin.krasuk@albany.company>
2021-04-24 18:05:50 -0300

    Fix ejercicio 1

--- 4-1.c
+++ 4-1.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
-printf("Ingrese 3 numeros distintos:");
-scanf("%d",&n1);
-printf("Ingrese otro numero:");
-scanf("%d",&n2);
-printf("Ingrese otro numero:");
-scanf("%d",&n3);
-if 
-{
-    (n1>n2 && n1>n3);
-    { printf("%d es mayor"),n1;
-    else
-        n2>n1 && n2>n3
-        printf("N2%d es mayor");
+    int n1,n2,n3;
+    printf("\nIngrese 3 numeros distintos:");
+    scanf("%d",&n1);
+    printf("\nIngrese otro numero:");
+    scanf("%d",&n2);
+    printf("\nIngrese otro numero:");
+    scanf("%d",&n3);
+    if(n1>n2 && n1>n3)
+    {
+
+        printf("\n%d es mayor\n",n1);
+
     }
-else
-    printf("n3%d es mayor");
-}
-system ("pause");
-return 0;
+    else if(n2>n1 && n2>n3)
+    {
+        printf("\n%d es mayor\n", n2);
+    }
+    else
+    {
+        printf("\n%d es mayor\n",n3);
+    }
+
+    system ("pause");
+    return 0;
 }
 
